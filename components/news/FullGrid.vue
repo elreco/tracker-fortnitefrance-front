f884c4cb89cf0fbaafa6ea69777e36b1cb04ee49
Alexandre LE CORRE <a.elreco@gmail.com>
2021-05-18 19:13:51 +0200

    gitflow-hotfix-stash: 1.2.18

--- components/news/FullGrid.vue
+++ components/news/FullGrid.vue
@@ -54,7 +54,7 @@ export default {
   watch: {
     async '$route.query'() {
       await this.$fetch()
-      window.scrollTo(0, 0)
+      this.$nextTick(() => window.scrollTo(0, 0))
     },
   },
   methods: {
